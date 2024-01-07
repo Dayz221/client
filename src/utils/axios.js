6ba2c0ccab02c652cc3b3e5ffd4ff345380ae7c6
Dayz221 <rutuchenal37@gmail.com>
2024-01-07 21:23:33 +0300

    final

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "https://server-todolist.vercel.app/api"
+    baseURL: "https://server-todolist-o2cb.onrender.com/api"
 })
 
 instance.interceptors.request.use(config => {
