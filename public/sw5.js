25dc0d9fc7bc247054e574411d7b6548d39f28e7
Dayz221 <rutuchenal37@gmail.com>
2024-01-07 17:44:33 +0300

    123

--- public/sw5.js
+++ public/sw5.js
@@ -1,10 +1,8 @@
 const version = "v5"
 
 log = msg => console.log(`${version}:${msg}`)
-console.log(1)
 
 self.addEventListener('push', event => {
-    сonsole.log(1)
     const data = event.data.json()
     const options = {
         body: data.body
