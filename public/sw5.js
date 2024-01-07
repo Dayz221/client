b230524c0b1b5b37e996e7cd46a9d50d5e76a5f3
Dayz221 <rutuchenal37@gmail.com>
2024-01-07 19:48:10 +0300

    1234

--- public/sw5.js
+++ public/sw5.js
@@ -5,7 +5,8 @@ log = msg => console.log(`${version}:${msg}`)
 self.addEventListener('push', event => {
     const data = event.data.json()
     const options = {
-        body: data.body
+        body: data.body,
+        vibrate: [100, 200, 100, 200, 100, 200]
     }
     let promise = self.registration.showNotification(data.title, options)
     event.waitUntil(promise)
